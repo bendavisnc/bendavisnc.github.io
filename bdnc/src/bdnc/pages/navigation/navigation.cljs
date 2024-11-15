6dba391ebd97ff1d971eab60341261a3732cf3af
bendavisnc <bendavisnc@gmail.com>
2024-11-15 13:58:40 -0500

    Introduces navigation links.

--- bdnc/src/bdnc/pages/navigation/navigation.cljs
+++ bdnc/src/bdnc/pages/navigation/navigation.cljs
@@ -1,4 +1,15 @@
 (ns bdnc.pages.navigation.navigation)
 
+(def links [{:name "about"
+             :path "/about"}
+            {:name "contacts"
+             :path "/contacts"}])
+
 (defn page []
- [:div {:id "page-navigation"} "todo - navigation content"])
+ [:div {:id "page-navigation"} 
+       [:ul (map (fn [link]
+                   [:li [:a {:href (:path link)}
+                            (:name link)]])
+                 links)]])
+ 
+         
