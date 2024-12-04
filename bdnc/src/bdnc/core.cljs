f3f37ca0c6e789c375dccb3501cea97789583e5f
bendavisnc <bendavisnc@gmail.com>
2024-12-04 14:55:19 -0500

    Adds additional test pages.

--- bdnc/src/bdnc/core.cljs
+++ bdnc/src/bdnc/core.cljs
@@ -4,9 +4,20 @@
 
 (enable-console-print!)
 
+(defn page-a []
+  [:div#a {:class ["bg-[#4686f2]", "h-dvh"]}])
+
+(defn page-b []
+  [:div#b {:class ["bg-[#fc05f6]", "h-dvh"]}])
+
+(defn page-c []
+  [:div#c {:class ["bg-[#fecd41]", "h-dvh"]}])
+
 
 (defn root []
-  [:div#a {:class ["bg-[#4686f2]", "h-dvh"]}])
+  [:<> [page-a]
+       [page-b]
+       [page-c]])
 
 (defn init! []
   (reagent-dom/render [root]
