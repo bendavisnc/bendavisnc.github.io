b216bd8c04a5bdd774cc27de507492afe11cfd76
bendavisnc <bendavisnc@gmail.com>
2024-12-05 14:10:24 -0500

    Adds event for scrolling.

--- bdnc/src/bdnc/core.cljs
+++ bdnc/src/bdnc/core.cljs
@@ -1,63 +1,65 @@
 (ns bdnc.core
     (:require [reagent.core :as reagent :refer [atom]]
               [reagent.dom :as reagent-dom]
+              [goog.string :as gstring]
+              [goog.string.format]
               [re-frame.core :as rf]))
 
 (enable-console-print!)
 
-;; // Target the element you want to observe
-;; const target = document.querySelector('.target');
-
-;; // Create an Intersection Observer
-;; const observer = new IntersectionObserver((entries) => {
-;;   entries.forEach(entry => {
-;;     if (entry.isIntersecting) {
-;;       console.log('The div is visible!');
-;;       // Perform your action here
-;;     } else {
-;;       console.log('The div is not visible.');
-;;     }
-;;   });
-;; }, {
-;;   root: null, // Observe within the viewport
-;;   threshold: 0.1 // Trigger when 10% of the element is visible
-;; });
-
-;; // Start observing the target
-;; observer.observe(target);
+(defn get-data-attribute [node key]
+  (when node
+    (aget (.-dataset node) key)))
 
 (defn scroll-observe! [target-selector]
   (let [target (.querySelector js/document target-selector)
         _ (when (nil? target)
             (throw (new js/Error "`scroll-observe!` target is null")))
         observer (new js/IntersectionObserver (fn [entries] 
-                                                (.forEach entries (fn [entry]
-                                                                    (if (.-isIntersecting entry) 
-                                                                      (println (str target-selector " is visibile"))
-                                                                      (println (str target-selector " is not visibile"))))))
-                                           (clj->js {:root nil 
-                                                     :threshold 0.1}))]
+                                                (doseq [entry entries
+                                                        :let [entry-target (.-target entry)]]
+                                                  (when (.-isIntersecting entry) 
+                                                    (rf/dispatch [:visible (get-data-attribute entry-target "index")]))))
+                                              (clj->js {:root nil 
+                                                        :threshold 0.1}))]
     (.observe observer target)))
+
+(defn title [i]
+  (str "wat" i))
+
+(rf/reg-event-db
+  :initialize
+  (fn [_ _]
+    {}))
+
+(rf/reg-event-db
+  :visible
+  (fn [db [_ i]]
+    (assoc db :title (title i))))
 
 (rf/reg-sub
   :title
   (fn [db _] 
-    "neat title"))
+    (:title db)))
 
 (defn header []
   (let [title @(rf/subscribe [:title])]
     [:div#header {:class ["bg-white", "w-dvw min-h-24 fixed top-0 left-0"]}
       [:span#title title]]))
 
+(defn page [id, color, index]
+  [:div {:id id
+         :class [(gstring/format "bg-[%s]" color), "h-dvh"]
+         :data-index index}])
+
 (defn page-a []
-  [:div#a {:class ["bg-[#4686f2]", "h-dvh"]}])
+  (page "a" "#4686f2" 0))
 
 (defn page-b []
-  [:div#b {:class ["bg-[#fc05f6]", "h-dvh"]}])
+  (page "b" "#fc05f6" 1))
 
 (defn page-c []
-  [:div#c {:class ["bg-[#fecd41]", "h-dvh"]}])
-
+  (page "c" "#fecd41" 2))
 
 (defn root []
   [:div#root-container {:class "relative"}
@@ -71,8 +73,8 @@
   (reagent-dom/render [root]
                       (.getElementById js/document 
                                        "app"))
-  (dorun (for [id ["#a", "#b", "#c"]]
-           (scroll-observe! id))))
+  (doseq [id ["#a", "#b", "#c"]]
+    (scroll-observe! id)))
 
 (defn on-js-reload [])
 
