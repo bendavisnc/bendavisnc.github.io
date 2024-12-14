eef94fd866add3c9e78a683100462ac981ebbc2b
bendavisnc <bendavisnc@gmail.com>
2024-12-14 17:06:31 -0500

    Tweaks hamburger component.

--- bdnc/src/bdnc/core.cljs
+++ bdnc/src/bdnc/core.cljs
@@ -59,7 +59,7 @@
   (let [title @(rf/subscribe [:title])]
     [:div#header {:class ["w-dvw", "min-h-24", "fixed", "top-0", "left-0", "bg-white", "bg-opacity-50", "flex", "justify-center", "items-end"]}
       [:span#title title]
-      [hamburger/component {:class "absolute top-2 right-2"}]])) 
+      [hamburger/component {:class ["absolute", "top-2", "right-2", "min-w-6", "flex", "justify-center", "cursor-pointer"]}]])) 
 
 (defn page [id, color]
   [:div {:id id
