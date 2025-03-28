40f0a51e2a75e6c9232e04f16253e7064453fe48
bendavisnc <bendavisnc@gmail.com>
2025-03-27 20:04:17 -0400

    Adds Make command for static site building.

--- Makefile
+++ Makefile
@@ -18,3 +18,16 @@ commit:
 clean:
 	@echo "Removing target directories."
 	cd bdnc; rm -rf ./target ./node_modules
+
+buildonce:
+	@echo "Building javascript."
+	cd bdnc; npm install; lein fig -- --build-once prod
+
+rollout: buildonce
+	@echo "Preparing static site."
+	rm -r bdncout
+	mkdir bdncout
+	cp -r bdnc/resources/public/ bdncout
+	mkdir bdncout/public/js
+	cp bdnc/target/public/cljs-out/prod/main_bundle.js bdncout/public/js/
+	mv bdncout/public/index_prod.html bdncout/public/index.html
--- bdnc/src/bdnc/core.cljs
+++ bdnc/src/bdnc/core.cljs
@@ -62,8 +62,8 @@
   [:div#root-container {:class ["relative" "w-dvw", "h-dvh", "overflow-hidden"]}
    [background/component
     {:class ["w-dvw", "h-dvh", "absolute", "z-[-99]"]}
-    {:image "/images/bg-hermansyah2.png"
-     :video "/videos/bg-loop.mp4"}]
+    {:image "./images/bg-hermansyah2.jpg"
+     :video "./videos/bg-loop.mp4"}]
    [:div#main-container {:class ["h-dvh", "overflow-auto", "snap-mandatory", "snap-y"]}
     [header/component {:class ["bg-[#00000010]", "2xl:bg-[#00000050]", "fixed", "flex", "h-[4rem]", "items-end" "justify-center", "left-0", "lg:h-[8rem]" "portrait:md:h-[8rem]", "top-0", "w-dvw" "z-[1]"]}]
     [pages-progress-circles {:id "pages-progress-circles"
